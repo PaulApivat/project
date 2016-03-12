f490e7b5ccfbba6f2f653658e25e0372700c98f7
Paul Apivat <paul.apivat@gmail.com>
2016-03-12 10:51:39 +0700

    add hour & min javascript

--- date.js
+++ date.js
@@ -1,2 +1,18 @@
-var today = new Date();
-document.getElementById('time').innerHTML=today;
+
+ var today = new Date();
+ document.getElementById('time').innerHTML=today;
+
+ var hour = today.getHours();
+ document.getElementById('hour').innerHTML=hour;
+
+ var min = today.getMinutes();
+ document.getElementById('min').innerHTML=min;
+
+//function timeNow(i){
+//	var d = new Date();
+//			h = (d.getHours()<10?'0':'') + d.getHours(),
+//			m = (d.getMinutes()<10?'0':'') + d.getMinutes();
+//	i.value = h + ':' + m;
+//}
+
+//document.getElementById(timeNow).innerHTML=today;
